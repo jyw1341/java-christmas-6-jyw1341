0b4cff60ea6e42d0064d1d4049082a9182f62481
정용우 <jyw1341@gmail.com>
2023-11-10 01:14:33 +0900

    feat(DayOfWeek): 요일 enum 추가

---
+++ src/main/java/christmas/enums/DayOfWeek.java
@@ -0,0 +1,36 @@
+package christmas.enums;
+
+public enum DayOfWeek {
+    SUNDAY(1, "일요일"),
+    MONDAY(2, "월요일"),
+    TUESDAY(3, "화요일"),
+    WEDNESDAY(4, "수요일"),
+    THURSDAY(5, "목요일"),
+    FRIDAY(6, "금요일"),
+    SATURDAY(7, "토요일");
+
+    private final int count;
+    private final String koreanName;
+
+    DayOfWeek(int count, String koreanName) {
+        this.koreanName = koreanName;
+        this.count = count;
+    }
+
+    public static DayOfWeek getToday(int count) {
+        for (DayOfWeek day : DayOfWeek.values()) {
+            if (day.getCount() == (count)) {
+                return day;
+            }
+        }
+        return null;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public String getKoreanName() {
+        return koreanName;
+    }
+}
