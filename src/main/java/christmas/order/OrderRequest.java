c247d15229f2f4ed4398bd04e347745cf1ac57ad
정용우 <jyw1341@gmail.com>
2023-11-09 23:52:27 +0900

    feat(OrderRequest): 이벤트 주의 사항 검증 기능 추가
    - 음료만 주문 불가 검증 기능 추가
    - 메뉴 최대 주문 검증 기능 추가

--- src/main/java/christmas/order/OrderRequest.java
+++ src/main/java/christmas/order/OrderRequest.java
@@ -1,14 +1,12 @@
 package christmas.order;
 
 import christmas.enums.Menu;
+import christmas.enums.MenuType;
 import utils.RegexExpression;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.EnumMap;
-import java.util.List;
+import java.util.*;
 
 import static utils.Validator.*;
 
@@ -18,6 +16,8 @@ public class OrderRequest {
     public static final String MENU_SEPARATOR = "-";
     public static final String ERROR_INVALID_DATE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
     public static final String ERROR_INVALID_MENU = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    public static final String ERROR_ONLY_BEVERAGE = "[ERROR] 음료만 주문 시, 주문할 수 없습니다.";
+    public static final String ERROR_OVER_MAX_ORDER = "[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.";
     public static final int MIN_DATE = 1;
     public static final int MAX_DATE = 31;
     private Date date;
@@ -45,7 +45,7 @@ public class OrderRequest {
     }
 
     public void setMenu(String input) {
-        validateMenu(input);
+        validateMenuInput(input);
         EnumMap<Menu, Integer> result = new EnumMap<>(Menu.class);
         for (String menuAndAmount : input.split(INPUT_SEPARATOR)) {
             String[] parts = menuAndAmount.split(MENU_SEPARATOR);
@@ -53,10 +53,11 @@ public class OrderRequest {
             Integer amount = Integer.parseInt(parts[1]);
             result.put(menu, amount);
         }
+        validateMenuEventConditions(result);
         this.menu = result;
     }
 
-    private void validateMenu(String input) {
+    private void validateMenuInput(String input) {
         validateIsNotBlank(input, ERROR_INVALID_MENU);
         List<String> menus = new ArrayList<>();
         for (String menuAmountPair : input.split(INPUT_SEPARATOR)) {
@@ -68,5 +69,25 @@ public class OrderRequest {
             validateCorrectFormat(amount, RegexExpression.NUMBER_GREATER_THAN_1, ERROR_INVALID_MENU);
         }
         validateIsElementUnique(menus, ERROR_INVALID_MENU);
+    }
+
+    private void validateMenuEventConditions(EnumMap<Menu, Integer> menus) {
+        validateOnlyBeverage(menus);
+        validateOverMaxOrder(menus);
+    }
+
+    private void validateOnlyBeverage(EnumMap<Menu, Integer> menus) {
+        Set<Menu> menuSet = menus.keySet();
+        long beverageCount = menuSet.stream().filter((menu) -> menu.getType().equals(MenuType.BEVERAGES)).count();
+        if (menuSet.size() == beverageCount) {
+            throw new IllegalArgumentException(ERROR_ONLY_BEVERAGE);
+        }
+    }
+
+    private void validateOverMaxOrder(EnumMap<Menu, Integer> menus) {
+        int total = menus.values().stream().mapToInt(Integer::intValue).sum();
+        if (total > 20) {
+            throw new IllegalArgumentException(ERROR_OVER_MAX_ORDER);
+        }
     }
 }
--- src/test/java/christmas/order/OrderRequestTest.java
+++ src/test/java/christmas/order/OrderRequestTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static christmas.order.OrderRequest.ERROR_INVALID_DATE;
-import static christmas.order.OrderRequest.ERROR_INVALID_MENU;
+import static christmas.order.OrderRequest.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
@@ -34,7 +33,7 @@ class OrderRequestTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"양송이수프-1", "티본스테이크-5", "초코케이크-10", "제로콜라-3"})
+    @ValueSource(strings = {"양송이수프-1", "티본스테이크-5", "초코케이크-10", "초코케이크-10,제로콜라-3"})
     @DisplayName("메뉴 입력이 올바른 형식이면 예외가 발생하지 않는다")
     public void testSetMenu1(String menu) {
         OrderRequest orderRequest = new OrderRequest();
@@ -64,7 +63,7 @@ class OrderRequestTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"양송이스프-1-1", "양송이스프-1a", "양송이스프--1", "양송이스프_1", "양송이스프#1", "양송이스프 1-"})
+    @ValueSource(strings = {"양송이수프-1-1", "양송이수프-1a", "양송이수프--1", "양송이수프", "양송이수프#1", "양송이수프 1-"})
     @DisplayName("주문 입력이 형식에 맞지 않으면 예외처리한다.")
     public void testSetMenu4(String input) {
         OrderRequest orderRequest = new OrderRequest();
@@ -75,7 +74,7 @@ class OrderRequestTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"양송이스프-1,양송이스프-1", "제로콜라-1,양송이스프-1,제로콜라-1"})
+    @ValueSource(strings = {"양송이수프-1,양송이수프-1", "제로콜라-1,양송이수프-1,제로콜라-1"})
     @DisplayName("메뉴를 중복해서 주문하면 예외처리한다.")
     public void testSetMenu5(String input) {
         OrderRequest orderRequest = new OrderRequest();
@@ -83,5 +82,27 @@ class OrderRequestTest {
             orderRequest.setMenu(input); // Valid date input
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ERROR_INVALID_MENU);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"제로콜라-3", "레드와인-1,제로콜라-3", "샴페인-2", "제로콜라-1,레드와인-1,샴페인-1"})
+    @DisplayName("음료만 주문 시 예외가 발생한다.")
+    void testSetMenu6(String input) {
+        OrderRequest orderRequest = new OrderRequest();
+        assertThatThrownBy(() -> {
+            orderRequest.setMenu(input); // Valid date input
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_ONLY_BEVERAGE);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"양송이수프-10,티본스테이크-10,레드와인-1"})
+    @DisplayName("20개가 넘는 메뉴를 주문하면 예외가 발생한다.")
+    void testSetMenu7(String input) {
+        OrderRequest orderRequest = new OrderRequest();
+        assertThatThrownBy(() -> {
+            orderRequest.setMenu(input); // Valid date input
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ERROR_OVER_MAX_ORDER);
     }
 }
