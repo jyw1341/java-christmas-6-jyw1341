a5434193bd2bc326534460e1deae2cf4dd146a97
정용우 <jyw1341@gmail.com>
2023-11-11 00:14:04 +0900

    refactor(SpecialEvent): 날짜 구하는 코드 리팩터링

--- src/main/java/christmas/event/SpecialEvent.java
+++ src/main/java/christmas/event/SpecialEvent.java
@@ -8,6 +8,8 @@ import java.util.Calendar;
 import java.util.Date;
 
 import static christmas.enums.DayOfWeek.SUNDAY;
+import static christmas.utils.CalenderUtils.getDayOfMonth;
+import static christmas.utils.CalenderUtils.getMonth;
 
 public class SpecialEvent extends Event {
     @Override
@@ -29,15 +31,7 @@ public class SpecialEvent extends Event {
     }
 
     private boolean isChristmas(Date date) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        int month = calendar.get(Calendar.MONTH); // 월 (0부터 시작)
-        int day = calendar.get(Calendar.DAY_OF_MONTH); // 일
-
-        int christmasMonth = Calendar.DECEMBER; // 12월
-        int christmasDay = 25;
-
-        return month == christmasMonth && day == christmasDay;
+        return getMonth(date) == Calendar.DECEMBER && getDayOfMonth(date) == 25;
     }
 
     @Override
